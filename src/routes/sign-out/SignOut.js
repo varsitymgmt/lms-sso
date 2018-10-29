f4aab13a0ee7c876e9dcf3cf95079089cf01883a
gachn <gaurav@egnify.com>
2018-10-29 12:15:40 +0530

    Sign out handled

--- src/routes/sign-out/SignOut.js
+++ src/routes/sign-out/SignOut.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {
-  getURLParams,
-  toggleLoader,
-  deleteCookie,
-  getCommonDomain,
-} from 'utils/HelperMethods';
+import { getURLParams, toggleLoader, deleteCookie } from 'utils/HelperMethods';
+import PropTypes from 'prop-types';
 import Loader from 'components/Loader';
 
 class SignOut extends React.Component {
+  static contextTypes = {
+    // common host
+    commonHost: PropTypes.string.isRequired,
+  };
   componentDidMount() {
     this.removeTokensAndRedirectToSignIn();
   }
@@ -15,7 +15,7 @@ class SignOut extends React.Component {
   removeTokensAndRedirectToSignIn = () => {
     toggleLoader(true);
     let host = getURLParams('host');
-    const domain = getCommonDomain();
+    const domain = this.context.commonHost;
     deleteCookie({ key: 'token', domain });
     deleteCookie({ key: 'accessControlToken', domain });
     deleteCookie({ key: 'email', domain });
