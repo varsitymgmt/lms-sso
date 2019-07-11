3d3833f406ac8fd988e253669de965604df09a00
Hari <hari@Kirans-MacBook-Pro.local>
2019-07-11 17:59:14 +0530

    redirect to issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import axios from 'axios';
 import ReactGA from 'react-ga';
 import PropTypes from 'prop-types';
-import history from '../history';
 import { config } from '../config/environment';
 // const actions = 0;
 
@@ -29,28 +27,6 @@ const ContextType = {
   commonHost: PropTypes.object.isRequired,
 };
 
-/**
- * The top-level React component setting context (global) variables
- * that can be accessed from all the child components.
- *
- * https://facebook.github.io/react/docs/context.html
- *
- * Usage example:
- *
- *   const context = {
- *     history: createBrowserHistory(),
- *     store: createStore(),
- *   };
- *
- *   ReactDOM.render(
- *     <App context={context}>
- *       <Layout>
- *         <LandingPage />
- *       </Layout>
- *     </App>,
- *     container,
- *   );
- */
 class App extends React.PureComponent {
   static propTypes = {
     context: PropTypes.shape(ContextType).isRequired,
@@ -68,50 +44,6 @@ class App extends React.PureComponent {
 
   getChildContext() {
     return this.props.context;
-  }
-
-  componentWillMount() {
-    axios.interceptors.request.use(
-      configuration => {
-        const token = localStorage.getItem('token');
-        if (token != null) {
-          configuration.headers.Authorization = `Bearer ${token}`; // eslint-disable-line
-        }
-        return configuration;
-      },
-      err => Promise.reject(err),
-    );
-    let redirectTo = null;
-    axios.interceptors.response.use(
-      response => response,
-      error => {
-        if (error.response && error.response.status === 401) {
-          let url = error.response.request
-            ? error.response.request.responseURL
-            : null;
-          if (url) {
-            url = new URL(url);
-            if (url && url.pathname !== '/auth/local') {
-              if (!redirectTo) {
-                const currentURL = new URL(window.location);
-                if (currentURL.pathname !== '/signin') {
-                  redirectTo = currentURL.pathname + currentURL.search;
-                }
-              }
-              localStorage.removeItem('token');
-              history.push(
-                `/signin?redirectTo=${encodeURIComponent(redirectTo || '/')}`,
-              );
-              setTimeout(() => {
-                redirectTo = null;
-              }, 1500);
-            }
-          }
-          return Promise.reject(error);
-        }
-        return Promise.reject(error);
-      },
-    );
   }
 
   render() {
