dda528d3a5a48f67a75a97fa5f034c6ffac46c89
Mohit <mohit@egnify.com>
2020-07-18 12:32:41 +0530

    express device removed

--- src/server.js
+++ src/server.js
@@ -36,8 +36,6 @@ import { config } from './config/environment';
 import schema from './api/graphql/schema';
 import { isAuthenticated, isAdmin } from './api/auth/auth.service';
 
-const device = require('express-device');
-
 const app = express();
 
 // enable compression
@@ -83,7 +81,6 @@ app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '32mb' }));
 app.use(bodyParser.json({ limit: '32mb' }));
-app.use(device.capture());
 
 require('./api/api/v1').default(app);
 
