2ad52a89b14f0b2869deea378b339947958dbc8a
Mohit <mohit@egnify.com>
2020-07-20 19:30:16 +0530

    accesscontroltoken validation removed

--- src/api/auth/auth.service.js
+++ src/api/auth/auth.service.js
@@ -88,10 +88,7 @@ export function isAuthenticated(
 
         redisData = JSON.parse(redisData);
 
-        if (
-          redisData.token !== req.headers.authorization ||
-          redisData.accessControlToken !== req.headers.accesscontroltoken
-        ) {
+        if (redisData.token !== req.headers.authorization) {
           return res.status(401).end();
         }
 
